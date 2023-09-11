f3c495e1c3163fd6eef221891768fb5c98b48599
yourcoldeyes <normatovmirakbar30@gmail.com>
2023-09-11 16:42:13 +0500

    make uppercase :rainbow:

--- toUppercase.js
+++ toUppercase.js
@@ -2,6 +2,11 @@
 Write a function which converts the input string to uppercase.
 */
 
+
+/* function declaration */ 
 function makeUpperCase(str) {
   return str.toUpperCase();
 }
+
+/* function expression */
+const makeUpperCase = (str) => str.toUpperCase();
