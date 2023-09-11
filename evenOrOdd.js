27cb8fb175054232f064c6dd07e9605cac08364b
yourcoldeyes <normatovmirakbar30@gmail.com>
2023-09-11 16:28:03 +0500

    Even Or Odd :ocean:

---
+++ evenOrOdd.js
@@ -0,0 +1,14 @@
+/* Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers. */
+
+// function declaration
+function evenOrOdd(num) {
+  if(num % 2 == 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
+
+
+// arrow function
+const evenOrOdd = (num) => (num % 2 == 0 ? "Even" : "Odd");
